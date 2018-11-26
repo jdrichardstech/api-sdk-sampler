093b0608bbe1b0f084d432ca6c24b59c7bd08a97
jdrichardstech <jdrichardstech@gmail.com>
2018-11-26 18:43:49 -0500

    update block deprecated APIs

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,43 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', null)
-})
+  res.render('index', null);
+});
 
-router.post('/:page', function(req, res, next){
-	var query = req.body.query
-	var type = req.body.type // search or timeline
-  var term = req.body.term
-  var location = req.body.location
+router.post('/:page', function(req, res, next) {
+  var query = req.body.query;
+  var type = req.body.type; // search or timeline
+  var term = req.body.term;
+  var location = req.body.location;
   // console.log("YELP: " + JSON.stringify(req.body.term))
-  if(location!=null){
-    if(location.indexOf(' ')>=0){
-      location= location.replace(' ','+')
-       console.log("NEW LOCATION: "+ JSON.stringify(location))
-    }
- }
-
-  if(term!=null){
-    if(term.indexOf(' ') >=0){
-      term =  term.replace(' ','+')
-      console.log("NEW TERM: "+ JSON.stringify(term))
+  if (location != null) {
+    if (location.indexOf(' ') >= 0) {
+      location = location.replace(' ', '+');
+      console.log('NEW LOCATION: ' + JSON.stringify(location));
     }
   }
 
-console.log("YELP: " + JSON.stringify(term) + ' ' + JSON.stringify(location))
-	var url = null
-	if (type == 'search'){
-		url = '/twitter/search?term='+query
-	}
-	if (type == 'timeline'){
-		url = '/twitter/timeline?username='+query
-	}
-  if(term !=null && location!=null){
-    url = "/yelp?term="+term+"&location="+location
+  if (term != null) {
+    if (term.indexOf(' ') >= 0) {
+      term = term.replace(' ', '+');
+      console.log('NEW TERM: ' + JSON.stringify(term));
+    }
   }
 
-	res.redirect(url)
+  console.log('YELP: ' + JSON.stringify(term) + ' ' + JSON.stringify(location));
+  var url = null;
+  if (type == 'search') {
+    url = '/twitter/search?term=' + query;
+  }
+  if (type == 'timeline') {
+    url = '/twitter/timeline?username=' + query;
+  }
+  if (term != null && location != null) {
+    url = '/yelp?term=' + term + '&location=' + location;
+  }
 
-})
+  res.redirect(url);
+});
 
-module.exports = router
+module.exports = router;
--- routes/yelp.js
+++ routes/yelp.js
@@ -1,50 +1,83 @@
 var express = require('express');
-var router = express.Router()
-var Yelp = require('yelp')
+var router = express.Router();
+// var Yelp = require('yelp');
+const yelp = require('yelp-fusion');
 
 /* GET home page. */
 
-
-
 router.get('/', function(req, res, next) {
-  var yelp = new Yelp({
-  consumer_key: '-ebkvPiYsEYX0CS6nZiLKA',
-  consumer_secret: 'WSl_nACZLdz-0eOSL1A215xEZ7I',
-  token: '4n6-cPnsuaEb9-_sOFrzYmnx13Xm3xz6',
-  token_secret: 'ry4fphZHiRoQgizp2G_KDhPe-hQ'
-	});
+  // var yelp = new Yelp({
+  //   consumer_key: '-ebkvPiYsEYX0CS6nZiLKA',
+  //   consumer_secret: 'WSl_nACZLdz-0eOSL1A215xEZ7I',
+  //   token: '4n6-cPnsuaEb9-_sOFrzYmnx13Xm3xz6',
+  //   token_secret: 'ry4fphZHiRoQgizp2G_KDhPe-hQ'
+  // });
 
+  var term = req.query.term;
+  var location = req.query.location;
+  const apiKey =
+    '-mIxBdaLL4T7-sx8DKBvbfd591oYjfvmLpDQHb9_yBB2rjRKBQMNSrin92afS9dgL6Uk6WVZXSyhG6p7Q0weVVZxDASuK7ARk_mQVRTIuZBe-1smn7AI2OcW5X19W3Yx';
 
-  var term = req.query.term
-  var location = req.query.location
+  const client = yelp.client(apiKey);
 
-  yelp.search({term:term, location:location})
-    .then(function (data) {
+  client
+    .search({
+      term: term,
+      location: location
+    })
+    // .then(response => {
+    //   console.log(response.jsonBody.businesses[0].name);
+
+    // })
+    // .catch(e => {
+    //   console.log(e);
+    // });
+    .then(function(data) {
+      console.log(JSON.stringify(data.jsonBody.businesses));
       // res.json(data);
       var content = {
         title: term + ' in ' + location,
         place: location,
-        data: data,
+        data: data.jsonBody.businesses
+      };
 
+      var format = req.query.format == null ? 'html' : req.query.format;
+      if (format == 'json') {
+        res.json(data);
+      } else {
+        res.render('yelp', content);
       }
-
-
-      var format = (req.query.format==null) ? 'html': req.query.format
-      if(format=='json'){
-        res.json(data)
-      }else{
-      res.render('yelp', content)
-    }
     })
 
-    .catch(function (err) {
+    .catch(function(err) {
       console.error(err);
-		});
+    });
+  // yelp
+  //   .search({ term: term, location: location })
+  //   .then(function(data) {
+  //     // res.json(data);
+  //     var content = {
+  //       title: term + ' in ' + location,
+  //       place: location,
+  //       data: data
+  //     };
+
+  //     var format = req.query.format == null ? 'html' : req.query.format;
+  //     if (format == 'json') {
+  //       res.json(data);
+  //     } else {
+  //       res.render('yelp', content);
+  //     }
+  //   })
+
+  //   .catch(function(err) {
+  //     console.error(err);
+  //   });
 });
 
-router.post('/', function(req, res, next){
-  var id = req.query.id
-  res.json(id)
-})
+router.post('/', function(req, res, next) {
+  var id = req.query.id;
+  res.json(id);
+});
 
 module.exports = router;
